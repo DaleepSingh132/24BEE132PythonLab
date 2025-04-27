b201d77332f4be44abebd7786e33f613748e1e80
DaleepSingh132 <24bee132@spt.pdpu.ac.in>
2025-04-27 16:03:31 +0530

    Add files via upload

---
+++ Lab8/L8Q1.py
@@ -0,0 +1,8 @@
+print("Name:Daleep Singh")
+print("Roll No.:24BEE132")
+list =["hello" ,"hi", "bye" ,"please"]
+set =set()
+for i in list:
+    set.add(i.upper())
+   
+print(f"The set in uppercase is {set}")
---
+++ Lab8/L8Q2.py
@@ -0,0 +1,15 @@
+print("Name:Daleep Singh")
+print("Roll No.:24BEE132")
+import random
+randomset = {random.randrange (15,45) for _ in range (10)}
+print(set)
+removeset= set()
+print(randomset)
+c=0
+for i in randomset :
+    if(i<30):
+        c+=1
+    else:
+        removeset.add(i)
+randomset-=removeset
+print(randomset)
